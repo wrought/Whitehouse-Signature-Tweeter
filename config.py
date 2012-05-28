908e297e865810709c7e170618ca1bf3ec5f0629
regiomontanus <mattsenate@gmail.com>
2012-05-27 18:09:31 -0700

    adds config.py with default #oamonday White House URL values but no twitter API credentials. If you're adding credentials, make a new branch and do not push the new branch to remote repo, unless private.

---
+++ config.py
@@ -0,0 +1,28 @@
+'''
+Add values here to customize this script
+'''
+database = 'signature.db' # something like 'blah.db'
+
+complete_flag = False #do a complete remote sweep?
+
+# URL pieces from whitehouse.gov petition
+wh_url_base = "https://wwws.whitehouse.gov/petition-tool/signatures/more/"
+wh_url_id1 = "4fafe312709f037653000011" # Need to Change this
+wh_url_num = 1
+wh_url_id2 = "4fbb32994bd504422b000039" # Need to change this, gets updated by the script after that.
+
+'''
+Twitter API and account details needed for Tweeting
+'''
+
+# The consumer keys can be found on your application's Details
+# page located at https://dev.twitter.com/apps (under "OAuth settings")
+    consumer_key=""
+    consumer_secret=""
+
+# The access tokens can be found on your applications's Details
+# page located at https://dev.twitter.com/apps (located 
+# under "Your access token")
+    access_token=""
+    access_token_secret=""
+
