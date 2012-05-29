29bc96ab96169e13873497eb5c7e0a5fe7752f5d
Jack LaBarba <brakit@gmail.com>
2012-05-28 21:13:05 -0700

    WIP tweeter.py thread-ification in progress

--- tweeter.py
+++ tweeter.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 import tweepy
+import threading
+import Queue
 
-class Tweeter:
+class Tweeter(threading.Thread):
     
 # The consumer keys can be found on your application's Details
 # page located at https://dev.twitter.com/apps (under "OAuth settings")
@@ -18,7 +20,8 @@ class Tweeter:
     auth = ""
     api = ""
     
-    def __init__(self, c_key, c_sec, a_tok, a_tok_s):
+    def __init__(self, c_key, c_sec, a_tok, a_tok_s, q)
+        threading.Thread.__init__(self)
         self.consumer_key = c_key
         self.consumer_secret = c_sec
         self.access_token = a_tok
@@ -31,6 +34,29 @@ class Tweeter:
         
         #test to see if logged in
         print self.api.me().name
+
+        self.q = q
+        self.exitFlag = False
+
+        self.msg_preamble = "#oamonday just signed by "
+        self.msg_postamble = " bit.ly/J7tSiV"
+    
+    #TODO
+    def run(self):
+        while not exitFlag:
+            people = ""
+            rightLength = False
+            while not rightLength:
+                currentLength = len(msg_preamble + people + msg_postamble)
+                next_person = self.q[-1]
+                nextLength = len(msg_preamble + add_to_msg(people, next_person) + ms_postamble)
+                
+    
+    def add_to_msg(self, items, next_item):
+        if len(items) == 0:
+            return next_item
+        else:
+            return items + ', ' + next_item
 
     def tweet(self, message):
         self.api.update_status(message)
