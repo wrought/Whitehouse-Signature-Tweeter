684daba1b346c358355916d74e9f94560be8f90b
Matt <msenate@plos.org>
2012-05-29 13:25:43 -0700

    adds new arguments to tweeter constructor

--- twitter_test.py
+++ twitter_test.py
@@ -3,6 +3,6 @@
 from tweeter import *
 from config import *
 
-tweetbot = Tweeter(consumer_key, consumer_secret, access_token, access_token_secret)
+tweetbot = Tweeter(consumer_key, consumer_secret, access_token, access_token_secret, msg_preamble, msg_postamble)
 
 tweetbot.tweet("What's crappenin' world? This is Jack's new twitter bot")
