5d13314b2fbf053ec10174d71e956651a88c089f
Jack LaBarba <brakit@gmail.com>
2012-05-29 20:00:57 -0700

    Updating twitter_test.py to test Queue functionality in tweeter.py

--- twitter_test.py
+++ twitter_test.py
@@ -2,7 +2,27 @@
 
 from tweeter import *
 from config import *
+import Queue
+import time
+import threading
 
-tweetbot = Tweeter(consumer_key, consumer_secret, access_token, access_token_secret, msg_preamble, msg_postamble)
+twitterQueue = Queue.Queue(100)
 
-tweetbot.tweet("What's crappenin' world? This is Jack's new twitter bot")
+exitEvent = threading.Event()
+
+tweetbot = Tweeter(consumer_key, consumer_secret, access_token, access_token_secret, 
+                   msg_preamble, msg_postamble, twitterQueue, exitEvent, 10)
+
+tweetbot.start()
+
+for i in range(30):
+    print "DEBUG: MAIN: twitterQueue.put(Jack " + str(i) + ")"
+    twitterQueue.put("Jack " + str(i))
+
+#while not twitterQueue.empty():
+#    print "DEBUG: MAIN: get:" + twitterQueue.get()
+
+raw_input("press enter to exit . . . ")
+exitEvent.set()
+
+
