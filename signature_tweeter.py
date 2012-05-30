7bc71c6d0ead14848334939e65a4367b9ab20805
Jack LaBarba <brakit@gmail.com>
2012-05-29 20:24:54 -0700

    Add tweeter worker initializing. MVP now!

--- signature_tweeter.py
+++ signature_tweeter.py
@@ -9,6 +9,7 @@ from config import *
 import os
 import Queue
 from parser import *
+from tweeter import *
 
 #open db connection
 #import sqlite3
@@ -28,7 +29,11 @@ exit_event = threading.Event()
 
 parser_thread = parser("parser", 20, wh_url_base, wh_url_id1, wh_url_id2, 
                        twitter_Queue, exit_event, database)
+tweetbot = Tweeter(consumer_key, consumer_secret, access_token, access_token_secret, 
+                   msg_preamble, msg_postamble, twitter_Queue, exit_event, 10)
+
 parser_thread.start()
+tweetbot.start()
 
 raw_input("Press enter to end . . .")
 exit_event.set()
