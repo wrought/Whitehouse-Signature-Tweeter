17f7398c1cd4d3064c2db9090935c8e8b71f2668
Matt <msenate@plos.org>
2012-06-19 13:49:08 -0700

    adds geocode, corrected script

--- geocode.py
+++ geocode.py
@@ -17,13 +17,22 @@ geona = geocoders.GeoNames()
 wikip = geocoders.MediaWiki("http://en.wikipedia.org/wiki/%s")
 
 # Replace this with sqlite query of unique locations
-locations = ["Los Angeles, CA", "Hoboken, NJ", ", FL"]
+locations = ["Los Angeles, CA", "Hoboken, NJ", ", FL", "Ontario, CA"]
 
 # loop through locations, make request and store in db only if needed
 for loc in locations:
-    place, (lat, lng) = googl.geocode(loc)
+    try: 
+        geocode_result = googl.geocode(loc, exactly_one=False)
+        for geocode_entry in geocode_result:
+             place, (lat, lng) = geocode_entry
+             print geocode_entry
+        logger.info(geocode_result)
+    except Exception as e:
+        print "\n\nSomething goofy, logging...\n\n"
+        logger.exception(e)
     # store in DB here
-    loc_sentence = "%s: %.5f, %.5f" % (place, lat, lng)
-    logger.info(loc_sentence)
-    print loc_sentence
+    #loc_sentence = "%s: %.5f, %.5f" % (place, lat, lng)
+    #logger.info(loc_sentence)
+    #print loc_sentence
+    geocode_result = None
 
